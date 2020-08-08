0791d6b04b489ed8bca887ad0f594b585fdbb13d
Molinux <cybercrowbr@gmail.com>
2020-08-07 21:38:17 -0300

    feat(mongodb): connect to MongoDB
    Created a MongoDB configuration, mongoose install, user model, MongoDB Compass connection and admin
    account.

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export default {
+  MONGODB_URL: 'mongodb+srv://molinero:omnistack@cluster0-jhhrp.mongodb.net/amazonas?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
+}
