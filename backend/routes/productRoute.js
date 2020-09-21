e4ae2c8eb499e2f484f8ee2a4a5e951a7a7eabf0
Molinux <cybercrowbr@gmail.com>
2020-09-21 19:20:02 -0300

    feat: :sparkles: Added filter and sort products field

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,19 @@
-import express from 'express';
+import express, { query } from 'express';
 import Product from '../models/productModel';
 import { isAdmin, isAuth } from '../util';
 
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-  const products = await Product.find({});
+  const category = req.query.category ? { category: req.query.category} : {};
+  const searchKeyword = req.query.searchKeyword ? {
+    name: {
+      $regex: req.query.searchKeyword,
+      $options: 'i'
+    }
+  } : {};
+  const sortOrder = req.query.sortOrder ? (req.query.sortOrder === 'lowest' ? { price: 1 } : { price:-1 }) : { _id: -1 };
+  const products = await Product.find({...category, ...searchKeyword}).sort(sortOrder);
   res.send(products);
 })
 
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,11 +4,11 @@ import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
   PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL } from "../constants/productConstants"
 import Axios from "axios";
 
-const listProducts = () => async (dispatch) => {
+const listProducts = (category = '', searchKeyword = '', sortOrder = '') => async (dispatch) => {
   try {
 
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await Axios.get("/api/products");
+    const { data } = await Axios.get("/api/products?category=" + category + "&searchKeyword=" + searchKeyword + "&sortOrder=" + sortOrder);
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   }
   catch (error) {
